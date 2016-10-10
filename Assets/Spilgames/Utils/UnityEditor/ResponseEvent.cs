fb4f20b4622b00ea68186770e8343d93dda9c406
sebastian <sebastian.vasile24@gmail.com>
2016-10-10 09:52:47 +0200

    [Unity] Fixed event sending and started on processing response

--- Assets/Spilgames/Utils/UnityEditor/ResponseEvent.cs
+++ Assets/Spilgames/Utils/UnityEditor/ResponseEvent.cs
@@ -9,8 +9,46 @@ namespace SpilGames.Unity.Utils.UnityEditor
 		public string action;
 		public string type;
 
-		public static string processResponseEvent(ResponseEvent responseEvent){
-			return null; // TODO
+		public static void Build(JSONObject response){
+
+			ResponseEvent responseEvent = new ResponseEvent();
+
+			responseEvent.eventName = response.GetField("name").ToString();
+
+			if(response.HasField("type")){
+				responseEvent.type = response.GetField("type").ToString();
+			}
+
+			if(response.HasField("action")){
+				responseEvent.action = response.GetField("action").ToString();
+			}
+
+			if(response.HasField("data")){
+				responseEvent.data = response.GetField("data");
+			}
+
+			switch(responseEvent.type)
+			{
+				case "advertisement":
+					break;
+				case "overlay":
+					break;
+				case "gameconfig":
+					break;
+				case "packages":
+					break;
+				case "notification":
+					break;
+				case "playerdata":
+					break;
+				case "gamedata":
+					break;
+				case "gamestate":
+					
+					break;
+				case "reward":
+					break;
+			}
 		}
 	}
 }
--- Assets/Spilgames/Utils/UnityEditor/SpilEvent.cs
+++ Assets/Spilgames/Utils/UnityEditor/SpilEvent.cs
@@ -14,15 +14,14 @@ namespace SpilGames.Unity.Utils.UnityEditor
 	public class SpilEvent : MonoBehaviour
 	{
 		public string eventName;
-		private JSONObject data = new JSONObject ();
+		public JSONObject data = new JSONObject ();
 		public JSONObject customData = new JSONObject ();
-		private string queued = "0";
-		private string ts;
-		private string debug = "&debugMode=true";
+		private int ts;
+//		private string debug = "&debugMode=true";
 		private string uid = Spil.SpilUserIdEditor;
 
 		public void Send() {
-			ts = (Time.time * 1000).ToString();
+			this.ts = (int)(Time.time * 1000);
 
 			this.StartCoroutine (SendCoroutine ());
 		}
@@ -35,23 +34,30 @@ namespace SpilGames.Unity.Utils.UnityEditor
 			requestForm.AddField ("data", this.data.ToString ());
 			if (!this.customData.IsNull) {
 				requestForm.AddField ("customData", this.customData.ToString ());
+			} else {
+				requestForm.AddField ("customData", "{}");
 			}
-			requestForm.AddField ("ts", this.ts);
+
+
+			requestForm.AddField ("ts", "1470057439857");
 			requestForm.AddField ("queued", 0);
+			requestForm.AddField ("debugMode", "true");
 
 			WWW request = new WWW ("https://apptracker.spilgames.com/v1/native-events/event/android/" + Spil.BundleIdEditor + "/" + this.eventName, requestForm);
 			yield return request;
 //			while (!request.isDone);	
 			if (request.error != null) {
-				Debug.LogError ("Error getting data: " + request.error);  
+				Debug.LogError ("Error getting data: " + request.error); 
+				Debug.LogError ("Error getting data: " + request.text);
+				ResponseEvent.Build(new JSONObject(request.text));
 			} else { 
 				JSONObject serverResponse = new JSONObject (request.text);
-				Debug.Log ("Data returned: " + serverResponse.ToString());
+				Debug.LogError ("Data returned: " + serverResponse.ToString());
 			}
 		}
 
 		private void AddDefaultParameters() {
-			this.data.AddField("uid", uid);
+			this.data.AddField("uid", this.uid);
 			this.data.AddField("locale", "en");
 			this.data.AddField("appVersion", "1.0");
 			this.data.AddField("apiVersion", SpilUnityImplementationBase.PluginVersion);
@@ -60,6 +66,7 @@ namespace SpilGames.Unity.Utils.UnityEditor
 			this.data.AddField("osVersion", "6.0");
 			this.data.AddField("deviceModel", "Editor");
 			this.data.AddField("timezoneOffset", "0");
+			this.data.AddField("tto", "200");
 			this.data.AddField("packageName", Spil.BundleIdEditor);
 			this.data.AddField ("sessionId", "deadbeef");
 		}
