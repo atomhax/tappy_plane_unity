0d664178b17406460f5d4a49c2140736e375fb88
Frank Slofstra <F.Slofstra@spilgames.com>
2016-11-18 14:45:12 +0100

    Unity repo changes merged

--- Assets/Spilgames/Spil.cs
+++ Assets/Spilgames/Spil.cs
@@ -1,6 +1,6 @@
 /*
  * Spil Games Unity SDK 2016
- * Version 2.2.4
+ * Version 2.2.6
  * 
  * If you have any questions, don't hesitate to e-mail us at info@spilgames.com
  * Be sure to check the github page for documentation and the latest updates
@@ -198,6 +198,15 @@ namespace SpilGames.Unity
 		public void AdNotAvailable (string type)
 		{
 			SpilUnityImplementationBase.fireAdNotAvailableEvent (type);
+		}
+
+		/// <summary>
+		/// This method is called by the native Spil SDK, it should not be used by developers.
+		/// Developers can subscribe to the Spil.Instance.OpenParentalGate event.
+		/// </summary>
+		public void OpenParentalGate ()
+		{
+			SpilUnityImplementationBase.fireOpenParentalGateEvent ();
 		}
 
 		/// <summary>
