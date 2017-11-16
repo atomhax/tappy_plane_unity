bffc403e63c3414c2debed6bfc037c0405a8f948
Sebastian Vasile <sebastian.vasile@spilgames.com>
2017-11-16 16:25:17 +0100

    - Bug fixes

--- Assets/Spilgames/Base/Implementation/SpilUnityImplementationBase.cs
+++ Assets/Spilgames/Base/Implementation/SpilUnityImplementationBase.cs
@@ -5,9 +5,7 @@ using SpilGames.Unity.Helpers.GameData;
 using System.Collections;
 using SpilGames.Unity.Json;
 using SpilGames.Unity.Base.SDK;
-using SpilGames.Unity.Base.UnityEditor;
 using SpilGames.Unity.Helpers.IAPPackages;
-using SpilGames.Unity.Helpers.PlayerData;
 
 namespace SpilGames.Unity.Base.Implementations {
     public abstract class SpilUnityImplementationBase{
@@ -1657,7 +1655,6 @@ namespace SpilGames.Unity.Base.Implementations {
 		public static void fireUserDataMergeConflict(string data) {
 			Debug.Log("SpilSDK-Unity fireUserDataMergeConflict");
 
-		    SpilLogging.Log(data);
 			MergeConflict mergeConflict = JsonHelper.getObjectFromJson<MergeConflict>(data);
 
 			if (Spil.Instance.OnUserDataMergeConflict != null) {
