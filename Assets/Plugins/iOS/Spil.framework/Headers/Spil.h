a9ef489117fec2c55a885018fefe6a732e2299ca
Frank Slofstra <F.Slofstra@spilgames.com>
2018-01-24 10:41:21 +0100

    GDPR_ENABLED flag removed for iOS.

--- Assets/Plugins/iOS/Spil.framework/Headers/Spil.h
+++ Assets/Plugins/iOS/Spil.framework/Headers/Spil.h
@@ -10,7 +10,6 @@
 #import "GAI.h"
 
 #define SPIL_SDK_VERSION @"2.7.4"
-#define GDPR_ENABLED true
 
 @class ImageContext;
 @class Spil;
@@ -203,7 +202,7 @@
  * Check if the privacy policy popup was completed by the user.
  * Present the popup if it was never completed by the user.
  */
-+(void)checkPrivacyPolicy;
++(void)checkPrivacyPolicy:(BOOL)isUnity;
 
 /**
  * Forwarding Delegate method to let the Spil framework know when the app was launched
