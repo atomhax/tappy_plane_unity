93afe9942a0ae7bcb2813e0475efb00642de2e41
Sebastian Vasile <sebastian.vasile24@gmail.com>
2018-08-01 11:09:38 +0200

    - Bug fixes

--- Assets/Spilgames/Helpers/DailyBonus/DailyBonusHelper.cs
+++ Assets/Spilgames/Helpers/DailyBonus/DailyBonusHelper.cs
@@ -11,19 +11,18 @@ namespace SpilGames.Unity.Helpers.DailyBonus {
         public static GameObject TempSpilDailyBonus;
 
         public IEnumerator DownloadDailyBonusAssets() {
-            //if (!Caching.IsVersionCached(DailyBonus.Url, Hash128.Parse(DailyBonus.Url)))
-            {
-                Debug.Log("[DB] Asset bundle downloading started: " + DailyBonus.Url);
+            if (!Caching.IsVersionCached(DailyBonus.Url, Hash128.Parse(DailyBonus.Url))){
+                SpilLogging.Log("[DB] Asset bundle downloading started: " + DailyBonus.Url);
                 UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(DailyBonus.Url, Hash128.Parse(DailyBonus.Url), 0);
                 yield return request.SendWebRequest();
 
-                Debug.Log("[DB] Asset bundle downloading succeeded: " + request.error);
+                SpilLogging.Log("[DB] Asset bundle downloading succeeded: " + request.error);
                 
                 AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(request);
                 bundle.LoadAllAssets();
                 SpilDailyBonus = bundle.LoadAsset<GameObject>("DailyBonus");
                 
-                Debug.Log("[DB] SpilDailyBonus found: " + (SpilDailyBonus != null));
+                SpilLogging.Log("[DB] SpilDailyBonus found: " + (SpilDailyBonus != null));
             }   
         }
 
